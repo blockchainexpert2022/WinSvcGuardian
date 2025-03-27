20c932be55f93a055400732a3a11aeb1ce2c6cba
WDAGUtilityAccount <blockchainexpert2022@protonmail.com>
2025-03-27 13:13:53 +0100

    I don't know why lol

--- WinSvcGuardian/obj/Release/net9.0/WinSvcGuardian.AssemblyInfo.cs
+++ WinSvcGuardian/obj/Release/net9.0/WinSvcGuardian.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinSvcGuardian")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2bda109880a94157d560033d3f2db8ad4684d17")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1d03f175efb4b1ef722b844f6ab19ea04557a26")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinSvcGuardian")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinSvcGuardian")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
